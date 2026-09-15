cda2cc0ba4b693bf696b14ab846f0d60e0befdec
ArsenKarapetyan03 <karapetyanarsen224@gmail.com>
2026-09-15 17:45:10 +0400

    Wrapped component by createPortal for correct working from everywhare

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, type ReactNode } from "react";
 import { cn } from "../../lib/utils.ts";
 import { X } from "lucide-react";
+import Teleport from "./Teleport.tsx";
 
 interface CustomModalProps {
 	content: {
@@ -76,46 +77,48 @@ export const CustomModal = (
 	}
 
 	return (
-		<div className="fixed inset-0 z-50 flex items-center justify-center p-4">
-			{/* Backdrop */}
-			<div
-				onClick={handleClose}
-				className={cn(
-					"absolute inset-0 bg-black/50 transition-opacity duration-300",
-					visible ? "opacity-100" : "opacity-0"
-				)}
-			/>
-
-			{/* Modal */}
-			<div
-				className={cn(
-					"relative w-full rounded-lg bg-white p-6 shadow-xl transition-all duration-300 ease-out",
-					sizeStyles.modal,
-					visible ? "scale-100 opacity-100 translate-y-0" : "scale-90 opacity-0 translate-y-10"
-				)}
-			>
-				{/* Close Button */}
-				<button
+		<Teleport>
+			<div className="fixed inset-0 z-50 flex items-center justify-center p-4">
+				{/* Backdrop */}
+				<div
 					onClick={handleClose}
-					className="absolute right-4 top-4 text-gray-500 transition hover:text-gray-900"
-				>
-					<X />
-				</button>
-
-				<div className="pr-6">
-					<h2 className={cn(
-						"font-semibold",
-						SIZES[size].contentTitle,
-						variant === "alert" && "text-red-600!"
+					className={cn(
+						"absolute inset-0 bg-black/50 transition-opacity duration-300",
+						visible ? "opacity-100" : "opacity-0"
 					)}
+				/>
+
+				{/* Modal */}
+				<div
+					className={cn(
+						"relative w-full rounded-lg bg-white p-6 shadow-xl transition-all duration-300 ease-out",
+						sizeStyles.modal,
+						visible ? "scale-100 opacity-100 translate-y-0" : "scale-90 opacity-0 translate-y-10"
+					)}
+				>
+					{/* Close Button */}
+					<button
+						onClick={handleClose}
+						className="absolute right-4 top-4 text-gray-500 transition hover:text-gray-900"
 					>
-						{content.contentTitle}
-					</h2>
-					<div className={cn("mt-3 text-gray-600", sizeStyles.contentBody)}>
-						{content.contentBody}
+						<X />
+					</button>
+
+					<div className="pr-6">
+						<h2 className={cn(
+							"font-semibold",
+							SIZES[size].contentTitle,
+							variant === "alert" && "text-red-600!"
+						)}
+						>
+							{content.contentTitle}
+						</h2>
+						<div className={cn("mt-3 text-gray-600", sizeStyles.contentBody)}>
+							{content.contentBody}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</Teleport>
 	);
 };
