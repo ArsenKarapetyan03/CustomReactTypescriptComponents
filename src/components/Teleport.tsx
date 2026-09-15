f4b9568e249158546cba6cf8a8aa4878e80171b7
ArsenKarapetyan03 <karapetyanarsen224@gmail.com>
2026-09-15 17:48:15 +0400

    Created createPortal hook wrapper for custom components

---
+++ src/components/Teleport.tsx
@@ -0,0 +1,28 @@
+import { type ReactNode, useLayoutEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
+interface TeleportProps {
+	children: ReactNode;
+	to?: string;
+	belongs?: string;
+}
+
+const Teleport = (
+	{
+		children,
+		to = "#root",
+	}: TeleportProps) => {
+
+	const [mountedElement, setMountedElement] = useState<Element | null>(null);
+
+	useLayoutEffect(() => {
+		const element = document.querySelector(to) || document.body;
+		setMountedElement(element);
+	})
+
+	if (!mountedElement) return null;
+
+	return createPortal(children,	mountedElement)
+}
+
+export default Teleport
